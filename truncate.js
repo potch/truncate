7b167733925a205273b9c3dc1076be1d9b63088b
Matthew Claypotch <potch@Matthews-MacBook-Pro.local>
2012-09-27 20:14:11 -0700

    first commit

---
+++ truncate.js
@@ -0,0 +1,73 @@
+function text(node) {
+    var cn = node.childNodes;
+    var t='';
+    if (cn.length) {
+        for (var i=0; i<cn.length; i++) {
+            t += text(cn[i]);
+        }
+    } else {
+        t = node.textContent;
+    }
+    return t;
+}
+
+var hasTruncation = (function() {
+    var shim = document.createElement('div');
+    shim.innerHTML = '<div style="text-overflow: ellipsis"></div>';
+    var s = shim.firstChild.style;
+    return 'textOverflow' in s || 'OTextOverflow' in s;
+})();
+
+function truncate(el, opts) {
+    opts = opts || {};
+    if (hasTruncation && (!opts.dir || opts.dir != 'v')) return this;
+    var showTitle = opts.showTitle || false;
+    var dir = (opts.dir && opts.dir[0]) || 'h';
+    var scrollProp = dir == "h" ? "scrollWidth" : "scrollHeight";
+    var offsetProp = dir == "h" ? "offsetWidth" : "offsetHeight";
+    var truncText = opts.truncText || "&hellip;";
+    var textEl = opts.textEl || el;
+    var split = [" ",""], counter, success;
+    var txt, cutoff, delim;
+    var oldtext = textEl.getAttribute("oldtext") || text(textEl);
+    textEl.setAttribute("data-oldtext", oldtext);
+    for (var i=0; i<split.length; i++) {
+        delim = split[i];
+        txt = oldtext.split(delim);
+        cutoff = txt.length;
+        success = false;
+        if (textEl.getAttribute("data-oldtext")) {
+            textEl.innerHTML = oldtext;
+        }
+        if ((el[scrollProp] - el[offsetProp]) < 1) {
+            el.removeAttribute("data-truncated", null);
+            break;
+        }
+        var chunk = Math.ceil(txt.length/2), oc=0, wid;
+        for (counter = 0; counter < 15; counter++) {
+            textEl.innerHTML = txt.slice(0,cutoff).join(delim) + truncText;
+            wid = (el[scrollProp] - el[offsetProp]);
+            if (cutoff < 1) {
+                break;
+            } else if (wid < 2 && chunk == oc) {
+                if (dir === 'h' || (delim === '' && el.scrollWidth < el.offsetWidth)) {
+                    success = true;
+                    el.setAttribute("data-truncated", true);
+                    break;
+                }
+            } else if (wid > 1) {
+                cutoff -= chunk;
+            } else {
+                cutoff += chunk;
+            }
+            oc = chunk;
+            chunk = Math.ceil(chunk/2);
+        }
+        if (success) break;
+    }
+    if (showTitle && oldtext != text(textEl)) {
+        textEl.setAttribute("title", oldtext);
+    }
+}
+
+
